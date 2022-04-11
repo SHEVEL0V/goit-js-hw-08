41ffb343e164aac7b48769e3c745ec9d2bed945b
Андрій Шевельов <ashevelevmti@gmail.com>
2022-04-12 00:06:41 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,9 +3,9 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const playerSetTine = 0 + localStorage.getItem('videoplayer-current-time');
 
 localStorage.setItem('videoplayer-current-time', 0);
-
 player.on(
   'timeupdate',
   throttle(e => {
@@ -13,4 +13,4 @@ player.on(
   }, 1000),
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(playerSetTine);
